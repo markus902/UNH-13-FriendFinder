989892d41a1d22de943367042bdd44aca6bf703d
Markus Markowich <markus902@gmx.de>
2019-09-26 22:44:15 -0400

    added dummy profiles

--- app/friends.js
+++ app/friends.js
@@ -1,5 +1,28 @@
 var profiles = [{
-    name: "Markus"
+    name: "Donald Trump",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+    score: [9, 10, 10, 1, 7, 8, 5, 3, 2, 2]
+}];
+var profiles = [{
+    name: "The Rock",
+    photo: "https://i.pinimg.com/originals/a4/48/4d/a4484db1e2c9ee67209e0d3e54142aef.jpg",
+    score: [4, 3, 6, 7, 8, 7, 7, 10, 2, 8]
+}];
+var profiles = [{
+    name: "George Clooney",
+    photo: "https://www.onthisday.com/images/people/george-clooney-medium.jpg",
+    score: [2, 8, 8, 5, 1, 8, 3, 1, 5, 10]
 }];
 
 module.exports = profiles;
+
+// Like fast food
+// argues a lot
+// Money is important
+// I like vounteering
+// I wanna have kids
+// I like to travel
+// Family is important for me
+// I love home depot
+// dancing
+// I love shopping
