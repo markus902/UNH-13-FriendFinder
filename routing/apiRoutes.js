2d3535fa342725469ba6fef80408d46680dc2592
Markus Markowich <markus902@gmx.de>
2019-09-28 21:17:20 -0400

    working get route

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,9 +1,12 @@
 var path = require("path");
 
+var friends = require("../data/friends.js")
+
+
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/home.html"));
+        res.json(friends);
         console.log("server runs")
     });
     app.post("/api/friends", function (req, res) {
