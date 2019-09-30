5c79f431d4bdc376148c795dd44c5635c6bb837d
Markus Markowich <markus902@gmx.de>
2019-09-29 23:08:19 -0400

    final routing

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,8 @@
 var path = require("path");
 
 var friends = require("../data/friends.js")
-var answers = require("../data/answers.js")
+// var answers = require("../data/answers.js")
+var answers = [];
 
 module.exports = function (app) {
 
@@ -12,28 +13,31 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         answers.push(req.body)
+        console.log(answers);
         let eval = [];
         let index;
         friends.forEach(elem => {
             let profile = elem.score;
-            let survey = answers[0].query
             let total = 0;
             let result = 0;
 
-            console.log(profile);
-            console.log(survey);
+            // console.log(profile);
+            // console.log(survey);
 
             for (i = 0; i < profile.length; i++) {
-                result = Math.abs(profile[i] - survey[i]);
+                result = Math.abs(profile[i] - answers[0].query[i]);
                 total = total + result;
             }
             eval.push(total);
-            result = Math.max(...eval);
+            result = Math.min(...eval);
             index = eval.indexOf(result);
         });
+        console.log("total: " + eval);
         res.json({
             friend: friends[index],
         });
-        answers = [];
+        answers = []; // reseting answers array
+        console.log(answers)
+
     });
 }
